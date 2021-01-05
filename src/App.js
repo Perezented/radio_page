27006bb3a9db3a227a5a866f14ca025d29c95850
Michael Perez <mperez6473@gmail.com>
2021-01-05 16:21:24 -0600

    App.js has the updated info on it so now it's not boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+import Header from "./components/header.js";
+import Banner from "./components/banner";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import Social from "./components/social";
+import DJS from "./components/djs";
+import Player from "./components/player";
+import Paragraph from "./components/paragraph";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Player />
+        <Banner />
+        <Social />
+        <DJS />
+        <Paragraph />
+      </div>
+    </Router>
   );
 }
 
