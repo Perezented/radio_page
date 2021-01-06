43dfc982eae290c99d65c6a1a500d06e363ec249
Michael Perez <mperez6473@gmail.com>
2021-01-06 09:25:28 -0600

    Header updated with links for the current page to slide to a specified part of the page.

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <nav>
-      <Link to="/">Pagina Principal</Link>
-      <Link to="/">Mixes Oficiales</Link>
-      <Link to="/">Radio Escuchas: num</Link>
+    <nav id="top">
+      <Link className="logo" to="#top">
+        Latin Music Radio Online
+      </Link>
+      <Link to="#chat">Chat Online</Link>
+      <Link to="#social">Social Media</Link>
+      <Link to="#mixes">Mixes Oficiales</Link>
+      <Link to="#contact">Contacto</Link>
+      <Link>Radio Escuchas: num</Link>
     </nav>
   );
 }
