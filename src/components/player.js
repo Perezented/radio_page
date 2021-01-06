f78925d49188d8e2dbae8e8c95e88ace54f0f3bf
Michael Perez <mperez6473@gmail.com>
2021-01-06 09:28:40 -0600

    Radio player on bottom added to be fixed on the screen

--- src/components/player.js
+++ src/components/player.js
@@ -1,7 +1,19 @@
 export default function Player() {
   return (
     <section className="player">
-      <h3>This will be the play button area</h3>
+      <div className="controls">
+        {" "}
+        <i className="fas fa-2x fa-step-backward"></i>
+        <i className="fas fa-2x fa-fast-backward"></i>
+        {/* <i className="fas fa-2x fa-backward"></i> */}
+        <i className="fas fa-2x fa-play"></i>
+        <i className="fas fa-2x fa-pause"></i>
+        <i className="fas fa-2x fa-stop"></i>
+        {/* <i className="fas fa-2x fa-forward"></i> */}
+        {/* <i className="fas fa-2x fa-fast-forward"></i> */}
+        <i className="fas fa-2x fa-step-forward"></i>
+        <input type="range" min="0" max="1" step=".01" className="slider" />
+      </div>
     </section>
   );
 }
