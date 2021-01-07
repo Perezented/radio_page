c8a1e608650353d2346b33ab6d0dd9d44ea638c7
Michael Perez <mperez6473@gmail.com>
2021-01-07 13:09:58 -0600

    Changed all links to a tags as it does not have multiple pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,14 @@
-import { Link } from "react-router-dom";
-
 export default function Header() {
   return (
-    <nav id="top">
-      <Link className="logo" to="#top">
+    <nav>
+      <a className="logo" href="#top">
         Latin Music Radio Online
-      </Link>
-      <Link to="#chat">Chat Online</Link>
-      <Link to="#social">Social Media</Link>
-      <Link to="#mixes">Mixes Oficiales</Link>
-      <Link to="#contact">Contacto</Link>
-      <Link>Radio Escuchas: num</Link>
+      </a>
+      <a href="#chat">Chat Online</a>
+      <a href="#social">Social Media</a>
+      <a href="#djs">Mixes Oficiales</a>
+      <a href="#contact">Contacto</a>
+      <a>Radio Escuchas: num</a>
     </nav>
   );
 }
