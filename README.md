a2899b68eac72314b5ef168d4c624df2651344df
Michael Perez <mperez6473@gmail.com>
2021-01-07 17:40:55 -0600

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# latin music radio online
--- src/components/banner.js
+++
@@ -1,8 +0,0 @@
-export default function Banner() {
-  return (
-    <>
-      <h1>Latin Music Radio Online</h1>
-      <div className="banner" id="top"></div>
-    </>
-  );
-}
